a5cbba598fbef51f6a462b36b0b00d654efac794
Ritesh Baindara <riteshbaindara25@gmail.com>
2025-04-17 16:55:11 +0530

    Initial commit: Add fortune.py (v1.0) and release workflow

--- fortune.py
+++ fortune.py
@@ -6,24 +6,23 @@ def main():
     mood = input("How are you feeling today? (happy/sad/neutral): ").lower()
     
     happy_fortunes = [
-    "🌟 Your fortune: Joy follows you like sunshine, Ritesh Baindara! Embrace the day. 🌟",
-    "🌈 Your fortune: Laughter will open new doors for you today,. 🌈",
-    "💫 Your fortune: The universe dances to your happy rhythm💫"
+    "✨ Your fortune: Joy follows you like sunshine, Ritesh Baindara! Embrace the day. ✨",
+    "✨ Your fortune: Laughter will open new doors for you today,. ✨",
+    "✨ Your fortune: The universe dances to your happy rhythm✨"
     ]
 
     sad_fortunes = [
-        "🌧️ Your fortune: Storms don’t last forever, Ritesh Baindara. A rainbow is near. 🌧️",
-        "🌙 Your fortune: Even the moon needs darkness to glow — stay strong. 🌙",
-        "🕯️ Your fortune: One small spark of hope can light up your entire path. 🕯️"
+        "✨ Your fortune: Storms do not last forever, Ritesh Baindara. A rainbow is near. ✨",
+        "✨ Your fortune: Even the moon needs darkness to glow — stay strong. ✨",
+        "✨ Your fortune: One small spark of hope can light up your entire path. ✨"
     ]
 
     neutral_fortunes = [
-        "🍃 Your fortune: Stillness brings clarity — trust your calm mind. 🍃",
-        "📚 Your fortune: Today holds quiet wisdom. Be open to what it teaches you. 📚",
-        "🔮 Your fortune: You’re aligned with balance — something unexpected yet good awaits, Ritesh Baindara. 🔮"
+        "✨ Your fortune: Stillness brings clarity — trust your calm mind. ✨",
+        "✨ Your fortune: Today holds quiet wisdom. Be open to what it teaches you. ✨",
+        "✨ Your fortune: You are aligned with balance — something unexpected yet good awaits, Ritesh Baindara. ✨"
     ]
 
-    
     if mood == "happy":
         print(random.choice(happy_fortunes))
     elif mood == "sad":
